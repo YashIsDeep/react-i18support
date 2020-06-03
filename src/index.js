09bf8275d3219ea17224a1fd8103bef995e0a74c
YashIsDeep <yashparth.gupta@gmail.com>
2020-06-03 14:13:09 +0530

    Added modularity

--- src/index.js
+++ src/index.js
@@ -17,16 +17,6 @@ function createElementFromString(str)
   return element;
 }
 
-function initRender()
-{
-  ReactDOM.render(
-    <React.StrictMode>
-      <App></App>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
 function timeLogger()
 {
   console.log("START");
@@ -45,10 +35,56 @@ function timeLogger()
   console.log("END")
 }
 
-var funBuffer={};
-let userLang='en'
-function init(){
-  i18next
+function initRender()
+{
+  ReactDOM.render(
+    <React.StrictMode>
+      <App></App>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+function loadPage()
+{
+  var end=Date.now();
+  console.log('i18nNext took '+(end-start)+' ms.');
+  initRender();
+  var isRendered=false;
+  var render;
+  //Initial render done
+
+  //Time logging function
+  i18next.changeLanguage("lg").then(timeLogger);
+
+  var funBuffer={};
+  const btn = document.getElementById('publisher');
+  btn.addEventListener('click',function(){
+    var selectedLanguagePreference=document.querySelector('input[name="lang"]:checked').value;
+    var selectedBundlePreference=document.querySelector('input[name="bundleOption"]:checked').value;
+    var start=Date.now();
+    if(isRendered) document.getElementById('content').removeChild(render);
+    i18next.changeLanguage(selectedLanguagePreference)
+      .then(()=>{
+        var mid=Date.now();
+        $script(hostUrl+'/bundles/bundle'+selectedBundlePreference+'.js', () => {
+          var end=Date.now();
+          console.log("Update took "+(mid-start)+" ms to change language and "+(end-mid)+" ms to load bundle.");
+
+          if(funBuffer[selectedBundlePreference]===undefined)funBuffer[selectedBundlePreference]=window.loadComponent;
+          var loader = funBuffer[selectedBundlePreference];
+          
+          var component=loader();
+          render=createElementFromString(component);
+          document.getElementById('content').appendChild(render);
+          isRendered=true;
+        })
+      });
+  });
+}
+
+function setupLanguage(userLang){
+  return i18next
     .use(i18nextXHRBackend)
     .init({
       lng:userLang,
@@ -60,41 +96,11 @@ function init(){
       }
     }, function(err, t){
     })
-    .then(()=>window.__=i18next.getFixedT())
-    .then(()=>{
-      var end=Date.now();
-      console.log('i18nNext took '+(end-start)+' ms.');
-      window.i18next=i18next;
-      initRender();
-      var isRendered=false;
-      var render;
-      //Initial render done
-
-      i18next.changeLanguage("lg").then(()=>timeLogger());//debug
-
-      const btn = document.getElementById('publisher');
-      btn.addEventListener('click',function(){
-        var selectedLanguagePreference=document.querySelector('input[name="lang"]:checked').value;
-        var selectedBundlePreference=document.querySelector('input[name="bundleOption"]:checked').value;
-        var start=Date.now();
-        if(isRendered) document.getElementById('content').removeChild(render);
-        i18next.changeLanguage(selectedLanguagePreference)
-          .then(()=>{
-            var mid=Date.now();
-            $script(hostUrl+'/bundles/bundle'+selectedBundlePreference+'.js', () => {
-              var end=Date.now();
-              console.log("Update took "+(mid-start)+" ms to change language and "+(end-mid)+" ms to load bundle.");
-              if(funBuffer[selectedBundlePreference]===undefined)funBuffer[selectedBundlePreference]=window.loadComponent;
-              else window.loadComponent=funBuffer[selectedBundlePreference];
-              var component=window.loadComponent();
-              render=createElementFromString(component);
-              document.getElementById('content').appendChild(render);
-              isRendered=true;
-            })
-          });
-      });
-    });
+    .then(()=>window.__=i18next.getFixedT()); 
 }
 
-init();
 
+
+
+let userLanguage='en'
+setupLanguage(userLanguage).then(loadPage);
